0e73b1ee6f2625f6714def52e2640962eddc6d2d
tonyctsiu <tony@ibonus.net>
2023-06-21 18:09:27 +0800

    added testing instruction

--- README.md
+++ README.md
@@ -4,7 +4,10 @@
 Usage: node dist/ticketCreate [source FamilySeed]
 
 #Create Check
-Usage: node dist/checkCreate [source FamilySeed] [destination: r-address] [XRP amount] [sequence no]
+Usage: node dist/checkCreate [source FamilySeed] [destination: r-address] [XRP amount] [ticket sequence no]
+
+#Create check with ticket sequence number
+Usage: node dist/checkCreateWithTicket [source FamilySeed] [destination: r-address] [XRP amount] [ticket sequence no]
 
 #Cash Check
 Usage: node dist/checkCash [check recipient's FamilySeed] [CheckId] [cash amount]
@@ -14,3 +17,13 @@ Usage: node dist/getAccountDetail [FamilySeed]
 
 #XRP payment
 Usage: node dist/payment [FamilySeed] [destinationAddr] XRP
+
+# Testing Procedure
+
+This project is made for testing the concept of sending check with ticket sequence. Do the following to test the concept
+
+execute: node dist/ticketcreate s#########(Family seed of the check creator)
+upon successful execution, it should return a new sequence number as follows:
+new sequence number = 38486563
+
+then execute: node dist/checkCreateWithTicket s#########s#########(Family seed of the check creator) r#####(r-address of check recipient) 1(arp amount for the check) 38486561 (the new ticket sequence number )
--- checkCreateWithTicket.ts
+++ checkCreateWithTicket.ts
@@ -4,9 +4,14 @@ import { utils, derive, sign } from "xrpl-accountlib";
 const main = async () => {
   const client = new XrplClient("wss://s.altnet.rippletest.net:51233/");
 
-  if (process.argv.length < 3) {
+  if (process.argv.length < 5) {
     console.log("create check to destination account");
-    console.log("Usage: node dist/ticketCreate <source FamilySeed> ");
+    console.log(
+      "Usage: node dist/checkCreateWithTicket <source FamilySeed> <destination: r-address> <XRP amount> [ticket sequence no]"
+    );
+    console.log(
+      "if sequence no is empty, the sequence no queried from blockchain is used."
+    );
     process.exit(1);
   }
 
@@ -23,62 +28,55 @@ const main = async () => {
     console.log("Error: ", data.result.error_message);
     process.exit(1);
   }
-
-  var sequence = data.account_data.Sequence;
-
+  console.log("account info: ", data);
+  var sequence = 0;
+  if (process.argv.length >= 6) {
+    sequence = parseInt(process.argv[5]);
+  }
   var { id, signedTransaction } = sign(
     {
-      TransactionType: "TicketCreate",
+      TransactionType: "CheckCreate",
       Account: account.address,
-      TicketCount: 1,
-      Sequence: sequence,
+      Destination: process.argv[3],
+      SendMax: String(parseFloat(process.argv[4]) * 1_000_000),
+      InvoiceID:
+        "6F1DFD1D0FE8A32E40E1F2C05CF1C15545BAB56B617F9C6C2D63A6B704BEF59B",
+      DestinationTag: 1,
+      TicketSequence: sequence,
+      Sequence: 0,
       Fee: "12",
     },
     account
   );
-  console.log("account info: ", data);
   console.log("id ", id);
   console.log("signedTransaction ", signedTransaction);
+
   var result = await client.send({
     command: "submit",
     tx_blob: signedTransaction,
   });
 
-  console.log("transaction send result: ", result);
+  console.log("submission ", result);
   if (result.engine_result_code != 0) {
     console.log("Exit on error");
     process.exit(1);
   }
-
+  console.log("waiting for finality");
+  // await sleep(10000);
   const subscribeData = await client.send({
     command: "subscribe",
-    accounts: [account.address],
+    accounts: [process.argv[3]],
   });
-  console.log("Subscribe: ", subscribeData);
   client.on("transaction", (data) => {
-    console.log("transaction result: ", JSON.stringify(data, null, 2));
-    if (data.engine_result_code != 0) {
-      process.exit(1);
-    }
-    let affectedNodes = data.meta.AffectedNodes;
-    console.log(
-      "Seeking new sequence number for affectNodes = ",
-      affectedNodes
-    );
-    if (affectedNodes == null) {
-      console.log("new ticket number not found");
-    }
-    affectedNodes.forEach((item: any) => {
-      if (item.hasOwnProperty("CreatedNode")) {
-        console.log(
-          "new sequence number =",
-          item.CreatedNode.NewFields.TicketSequence
-        );
-      }
-    });
+    console.log("transaction result: ", data);
     process.exit(1);
   });
-  //  process.exit(1);
 };
+
+function sleep(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
 main();
